112ec5b9d7a3bd853d4b0cbcbb62f0d790cf4264
sendon1982 <sendon1982@gmail.com>
2015-01-02 10:40:51 +1100

    add log for json request

---
+++ tripadviator-service/src/main/java/com/tripadviator/serivce/rest/BaseObjectMapper.java
@@ -0,0 +1,13 @@
+package com.tripadviator.serivce.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class BaseObjectMapper extends ObjectMapper
+{
+    private static final long serialVersionUID = -3343764657503640721L;
+
+    public BaseObjectMapper()
+    {
+
+    }
+}
