6b8bba1d8c454dcf97d5766e922257d195bd8b27
sendon1982 <sendon1982@gmail.com>
2015-01-06 09:49:56 +1100

    add a new project

--- tripadviator-service/src/main/java/com/tripadviator/serivce/product/ProductService.java
+++ tripadviator-service/src/main/java/com/tripadviator/serivce/product/ProductService.java
@@ -1,10 +1,14 @@
 package com.tripadviator.serivce.product;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tripadviator.domain.ProductDetail;
 import com.tripadviator.serivce.product.request.ProductDetailRequest;
+import com.tripadviator.serivce.product.response.ProductDetailResponse;
 import com.tripadviator.serivce.rest.RestClient;
 
 /**
@@ -21,8 +25,21 @@ public class ProductService
 	@Autowired
 	private RestClient restClient;
 	
+	/**
+	 * Get product detail information by productCode
+	 * 
+	 * @param url
+	 * @param request
+	 * @return
+	 */
 	public ProductDetail getProductDetailByCode(String url, ProductDetailRequest request)
 	{
-		return restClient.getProductByCode(url, request).getData();
+		Map<String, String> requestMap = new LinkedHashMap<String, String>();
+		requestMap.put("code", request.getCode());
+		requestMap.put("currencyCode", request.getCurrencyCode());
+		
+		ProductDetailResponse response = restClient.getRequest(url, requestMap, ProductDetailResponse.class);
+		
+		return response.getData();
 	}
 }
