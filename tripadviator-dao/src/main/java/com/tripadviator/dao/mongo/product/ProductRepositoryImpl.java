0a90c5bbcdf5168312593aae634c4aced09e1116
sendon1982 <sendon1982@gmail.com>
2015-04-24 12:09:07 +1000

    test

--- tripadviator-dao/src/main/java/com/tripadviator/dao/mongo/product/ProductRepositoryImpl.java
+++ tripadviator-dao/src/main/java/com/tripadviator/dao/mongo/product/ProductRepositoryImpl.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
