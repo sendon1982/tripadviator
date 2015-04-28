a3f2e72f849b09b6f470fe182f3061e58fe12376
sendon1982 <sendon1982@gmail.com>
2015-04-28 17:34:58 +1000

    test

--- tripadviator-web/src/main/java/com/tripadviator/web/util/TestUtil.java
+++ tripadviator-web/src/main/java/com/tripadviator/web/util/TestUtil.java
@@ -7,11 +7,14 @@ import org.springframework.http.MediaType;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tripadviator.web.constant.WebConstant;
 
 public class TestUtil 
 {
-    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
-    		MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+    		MediaType.APPLICATION_JSON.getType(), 
+    		MediaType.APPLICATION_JSON.getSubtype(), 
+    		Charset.forName(WebConstant.CHARSET_UTF8));
  
     public static byte[] convertObjectToJsonBytes(Object object) throws IOException 
     {
